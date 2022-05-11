f27944795643b4796735f1a7f710eeeeac763c56
N <46265532+NDom23@users.noreply.github.com>
2022-05-10 22:40:47 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class Main  {
+	
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		
+	
+		// JTextField 
+		
+		MyFrame frame = new MyFrame();
+		
+	}
+	
+	
+}
